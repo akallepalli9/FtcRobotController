2594d441fc5d218b7623e3512c6ceda3b8ddfc3b
akallepalli9 <akallepalli9@gmail.com>
2026-08-27 16:57:15 -0500

    Save local modifications before merging from shortcircuit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/StarterBotChassisTeleop.java
@@ -0,0 +1,165 @@
+package org.firstinspires.ftc;
+
+import static com.qualcomm.robotcore.hardware.DcMotor.ZeroPowerBehavior.BRAKE;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+/*
+ * This file includes a teleop (driver-controlled) file for the goBILDA® StarterBot Chassis/Intake for the
+ * 2026-2027 FIRST® Tech Challenge. It leverages a differential/Skid-Steer system for robot mobility,
+ * one motor driving an intake roller, and two servos which pull elements out of corners.
+ */
+
+@TeleOp(name = "StarterBot Teleop V2", group = "StarterBot")
+//@Disabled
+public class StarterBotChassisTeleop extends OpMode {
+
+    // Declare OpMode members.
+    private DcMotor leftDrive = null;
+    private DcMotor rightDrive = null;
+    private DcMotor intake = null;
+    private CRServo leftIntakeServo = null;
+    private CRServo rightIntakeServo = null;
+
+    // Set up a varable for each drive wheel to save power level for telemetry.
+    double leftPower;
+    double rightPower;
+
+    double power = .20;
+    // Create a variable to set to the intake.
+    double intakePower;
+
+    /*
+     * Code to run ONCE when the driver hits INIT
+     */
+    @Override
+    public void init() {
+
+        /*
+         * Initialize the hardware variables. Note that the strings used here as parameters
+         * to 'get' must correspond to the names assigned during the robot configuration
+         * step.
+         */
+        leftDrive = hardwareMap.get(DcMotor.class, "left_drive");
+        rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
+        intake = hardwareMap.get(DcMotorEx.class, "intake");
+        leftIntakeServo = hardwareMap.get(CRServo.class, "left_intake_servo");
+        rightIntakeServo = hardwareMap.get(CRServo.class, "right_intake_servo");
+
+        /*
+         * To drive forward, most robots need the motor on one side to be reversed,
+         * because the axles point in opposite directions. Pushing the left stick forward
+         * MUST make robot go forward. So adjust these two lines based on your first test drive.
+         * Note: The settings here assume direct drive on left and right wheels. Gear
+         * Reduction or 90 Deg drives may require direction flips
+         */
+        leftDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightDrive.setDirection(DcMotor.Direction.REVERSE);
+
+        /*
+         * Setting zeroPowerBehavior to BRAKE enables a "brake mode". This causes the motor to
+         * slow down much faster when it is coasting. This creates a much more controllable
+         * drivetrain. As the robot stops much quicker.
+         */
+        leftDrive.setZeroPowerBehavior(BRAKE);
+        rightDrive.setZeroPowerBehavior(BRAKE);
+        intake.setZeroPowerBehavior(BRAKE);
+
+        /*
+         * set Feeders to an initial value to initialize the servo controller
+         */
+        leftIntakeServo.setPower(-.9);
+        rightIntakeServo.setPower(.9);
+
+        /*
+         * Much like our drivetrain motors, we set the right intake servo to reverse so that both
+         * servos work to pull elements into the intake.
+         */
+        rightIntakeServo.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        /*
+         * Tell the driver that initialization is complete.
+         */
+        telemetry.addData("Status", "Initialized");
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits INIT, but before they hit START
+     */
+    @Override
+    public void init_loop() {
+    }
+
+    /*
+     * Code to run ONCE when the driver hits START
+     */
+    @Override
+    public void start() {
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits START but before they hit STOP
+     */
+    @Override
+    public void loop() {
+        /*
+         * Here we call a function called arcadeDrive. The arcadeDrive function takes the input from
+         * the joysticks, and applies power to the left and right drive motor to move the robot
+         * as requested by the driver. "arcade" refers to the control style we're using here.
+         * Much like a classic arcade game, when you move the left joystick forward both motors
+         * work to drive the robot forward, and when you move the right joystick left and right
+         * both motors work to rotate the robot. Combinations of these inputs can be used to create
+         * more complex maneuvers.
+         */
+        arcadeDrive(-gamepad1.left_stick_y * power, gamepad1.right_stick_x * power);
+
+        /*
+         * Set the intake power variable to equal the right trigger, minus the left trigger.
+         * Each trigger outputs a signal from 0-1, with 0 as fully released, and 1 fully depressed.
+         * This gives us proportional control of the intake speed. The speed increases as we pull
+         * the right trigger further. It's occasionally helpful to be able to reverse the intake,
+         * so we also factor in the the left trigger. If the left trigger is fully depressed,
+         * the intakePower variable will be -1. If the right trigger is fully depressed, the variable
+         * will be 1. If the driver pulls both triggers, the intake will remain off.
+         * We use this technique (creating a variable, and setting it to our control inputs) to
+         * allow us to avoid setting the same motors/servos power more than once per loop. That can
+         * create erratic behavior.
+         */
+        intakePower = gamepad2.right_trigger - gamepad2.left_trigger;
+
+        intake.setPower(intakePower);
+        leftIntakeServo.setPower(intakePower);
+        rightIntakeServo.setPower(intakePower);
+
+        /*
+         * Show motor powers on the Driver Station via telemetry.
+         */
+        telemetry.addData("Motorsv2", "left (%.2f), right (%.2f)", leftPower, rightPower);
+        telemetry.addData("Triggers", "left (%.2f, right (%.2f)",gamepad1.left_trigger, gamepad1.right_trigger);
+
+    }
+
+    /*
+     * Code to run ONCE after the driver hits STOP
+     */
+    @Override
+    public void stop() {
+    }
+
+    void arcadeDrive(double forward, double rotate) {
+        leftPower = forward + rotate;
+        rightPower = forward - rotate;
+
+        /*
+         * Send calculated power to wheels
+         */
+        leftDrive.setPower(leftPower);
+        rightDrive.setPower(rightPower);
+    }
+}
